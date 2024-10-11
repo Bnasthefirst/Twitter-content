8ca71f7c08df139dd4c6fa506e98917bfad934bd
Bnasthefirst <150389377+Bnasthefirst@users.noreply.github.com>
2024-10-11 10:10:21 +0100

    Update simplenft.sol

--- October11th/simplenft.sol
+++ October11th/simplenft.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SimpleNFT is ERC721, Ownable {
+    uint256 public tokenCounter;
+
+    constructor() ERC721("SimpleNFT", "SNFT") {
+        tokenCounter = 0;
+    }
+
+    function mintNFT(address recipient) public onlyOwner {
+        _safeMint(recipient, tokenCounter);
+        tokenCounter++;
+    }
+}
