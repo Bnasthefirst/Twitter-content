1d0e6e147b4d8f78cb0f471bcb4aece4b3bd3992
Bnasthefirst <150389377+Bnasthefirst@users.noreply.github.com>
2024-10-13 12:07:47 +0100

    Create module.py

---
+++ October13th/module.py
@@ -0,0 +1,26 @@
+import math # Imports the entire math module
+print (math.sqrt (16)) # Accessing a function in the math module
+
+
+from math import sqrt # Imports only the sqrt function
+print(sqrt (16)) # No need to use math.sqrt ()
+
+from collections import deque #
+Imports the deque class
+d = deque ([1, 2, 31)
+
+from math
+import pi
+# Importing a
+constant print (pi)
+# Prints the value of pi
+(3.14159...)
+
+import os.path # Imports a submodule print(os.path.basename ('/home/user/ file.txt'))
+# Accessing a function
+in the submodule
+
+
+from math import sqrt, ceil, floor  # Importing multiple functions from the math module
+
+from math import * # Import everything from the math module print(sqrt (16)) # sqrt is available directly
