9fb7576a18dbbfc8cb71b66e261a161372b0e386
Bnasthefirst <150389377+Bnasthefirst@users.noreply.github.com>
2024-10-16 15:20:05 +0100

    Create deques.py

---
+++ October16th/deques.py
@@ -0,0 +1,20 @@
+from collections import deque
+d = deque ([1, 2, 31)
+
+
+d. append (4) # Appends 4 to the right end
+d. appendleft (0) # Appends 0 to the left end.
+
+d. pop(4) #  Removes and returns the rightmost element.
+d. popleft(0) #Removes and returns the leftmost element.
+
+
+d.extend ([4, 51]) # Adds multiple elements to the right end.
+
+d. extendleft([10, -11]) # Adds multiple elements to the left end (reverses the input order).
+
+
+ rotate (1) #Rotates the deque two steps to the right.
+ rotate(-1) #Rotates one step to the left.
+
+d2 = deque(maxlen=10) 
