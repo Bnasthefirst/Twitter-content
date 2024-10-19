6716f3ffa3c4a83ff887e861f6b28d8e4a4f710f
Bnasthefirst <150389377+Bnasthefirst@users.noreply.github.com>
2024-10-19 12:17:15 +0100

    Create if&else.py

---
+++ October19th/if&else.py
@@ -0,0 +1,29 @@
+x = 10
+if x > 5:
+    print("x is greater than 5")
+
+
+y = 2
+if y > 5:
+    print("x is greater than 5")
+else:
+    print("x is less than or equal to 5")
+
+
+m = 10
+if m > 10:
+    print("m is greater than 10")
+elif m == 10:
+    print("m is equal to 10")
+else:
+    print("m is less than 10")
+
+
+w = 7
+if w > 5:
+    if w < 10:
+        print("w is between 5 and 10")
+    else:
+        print("w is greater than or equal to 10")
+else:
+    print("w is 5 or less")
