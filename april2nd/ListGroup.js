47c8bc538371be03481886fdcb276190b0d8a9d3
bnascrypt <150389377+Bnasthefirst@users.noreply.github.com>
2025-04-02 14:03:13 +0100

    Create ListGroup.js

---
+++ april2nd/ListGroup.js
@@ -0,0 +1,43 @@
+import { Fragment } from "react";
+function ListGroup() {
+
+   
+    return (
+        <>
+        
+        <Fragment>
+                    <div>
+            <h1> this is a list group </h1>
+
+            <ul className="list">
+
+                <li className="listitems">
+                    pedro
+                </li>
+
+                <li className="listitems">
+                    bnas
+                </li>
+                <li className="listitems">
+                    quavo
+                </li>
+
+                <li className="listitems">
+                    kendrick
+                </li>
+            </ul>
+
+        </div>
+
+
+        </Fragment>
+        
+        
+        </>
+
+
+
+    )
+}
+
+export default ListGroup;
