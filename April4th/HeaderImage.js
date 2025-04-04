304c91097d7068639908ab71b4df65df10e2fae2
bnascrypt <150389377+Bnasthefirst@users.noreply.github.com>
2025-04-04 13:14:48 +0100

    Create HeaderImage.js

---
+++ April4th/HeaderImage.js
@@ -0,0 +1,26 @@
+import image1 from "./Logo.png";
+function Header()
+{
+return (
+    <div>
+         
+    <p>this is what i want to do with my life </p> 
+    <img src ={image1} alt = "sauce ">
+   
+    </img>
+    
+         
+    
+
+
+    </div>
+
+
+);
+       
+    
+
+
+}
+
+export default Header;
