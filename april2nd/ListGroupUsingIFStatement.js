42af9e4bc1896d6e430139ca0bea6c84bf6e7eb3
bnascrypt <150389377+Bnasthefirst@users.noreply.github.com>
2025-04-04 13:23:36 +0100

    Create ListGroupUsingIFStatement.js

---
+++ april2nd/ListGroupUsingIFStatement.js
@@ -0,0 +1,48 @@
+import { Fragment } from "react";
+function ListGroup() {
+
+
+    let items = [
+          
+          "front " ,
+          "back ", 
+          "truth ",
+          "trey",
+          "throw",
+    ];
+
+    items =[];
+
+
+
+   if (items.length ===0)
+    return (<> <p> there are no items </p></>);
+;
+
+    
+    return (
+        <>
+        
+        <Fragment>
+                    <div>
+            <h1> this is a list group </h1>
+
+            <ul className="list">
+
+            {items.map(item => <li key ={item}> {item}</li>)}
+            </ul>
+
+        </div>
+
+
+        </Fragment>
+        
+        
+        </>
+
+
+
+    )
+}
+
+export default ListGroup;
